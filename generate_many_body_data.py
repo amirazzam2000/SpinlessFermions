a5d25fc45c55669bf9b482a50f822124bdbf93e2
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-03 17:02:27 +0200

    updating cuda

--- generate_many_body_data.py
+++ generate_many_body_data.py
@@ -9,7 +9,7 @@ torch.set_printoptions(4)
 torch.backends.cudnn.benchmark=True
 torch.set_default_dtype(torch.float32)
 
-device = torch.device('cuda')
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 dtype = str(torch.get_default_dtype()).split('.')[-1]
 
 sys.path.append("./src/")
