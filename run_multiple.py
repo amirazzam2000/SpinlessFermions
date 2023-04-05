b1adf214d98bd5dc3ac0c3499a938fcda2a3a504
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-05 15:26:10 +0200

    saving the model after training

--- run_multiple.py
+++ run_multiple.py
@@ -4,4 +4,4 @@ model_name = "./partial_data/model-1"
 os.system("python3 run.py -V 10 -S 0.3 -M {}".format(model_name))
 
 
-os.system("python3 run.py -V 5 -S 0.3 -LM {}".format(model_name))
+os.system("python3 run.py -V 5 -S 0.3 -LM {} -M {}".format(model_name, model_name))
