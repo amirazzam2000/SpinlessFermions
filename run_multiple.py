e32f876c99bef5d33441bb0fe0f3d259997cc05d
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-09 19:18:55 +0200

    increasing number of epochs

--- run_multiple.py
+++ run_multiple.py
@@ -3,9 +3,9 @@ import sys, os
 model_name = "./partial_data/model-pre-twice"
 directory = "results/energy/data/pre_twice"
 
-os.system("python3 run.py -V 10 -S 0.3 -M {} -DIR {}".format(model_name, directory))
+os.system("python3 run.py -V 10 -S 0.3 -M {} --epochs 100000 -DIR {}".format(model_name, directory))
 
 os.system(
-    "python3 run.py -V 0 -S 0.3 -M {} -DIR {} --preepochs 0".format(model_name, directory))
+    "python3 run.py -V 0 -S 0.3 -M {} -DIR {} --epochs 100000 --preepochs 0".format(model_name, directory))
 
 os.system("python3 run.py -V 5 -S 0.3 -LM {} --epochs 100000 --preepochs 0 -DIR {}".format(model_name, directory))
