604648f1211e36ecf0a36cf82d3faf26339a3030
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-09 18:46:37 +0200

    adding another pre-training step

--- run_multiple.py
+++ run_multiple.py
@@ -1,9 +1,10 @@
 import sys, os
 
 model_name = "./partial_data/model-1"
-directory = "results/energy/data/pre"
+directory = "results/energy/data/pre_twice"
 
 os.system("python3 run.py -V 10 -S 0.3 -M {} -DIR {}".format(model_name, directory))
 
+os.system("python3 run.py -V 0 -S 0.3 -M {} -DIR {}".format(model_name, directory))
 
 os.system("python3 run.py -V 5 -S 0.3 -LM {} --epochs 100000 -DIR {}".format(model_name, directory))
