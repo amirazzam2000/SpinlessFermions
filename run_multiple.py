8b05925c1312902273ee6401492fd586ab369728
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-09 19:17:09 +0200

    remove pre-training for the later layers

--- run_multiple.py
+++ run_multiple.py
@@ -5,6 +5,7 @@ directory = "results/energy/data/pre_twice"
 
 os.system("python3 run.py -V 10 -S 0.3 -M {} -DIR {}".format(model_name, directory))
 
-os.system("python3 run.py -V 0 -S 0.3 -M {} -DIR {}".format(model_name, directory))
+os.system(
+    "python3 run.py -V 0 -S 0.3 -M {} -DIR {} --preepochs 0".format(model_name, directory))
 
-os.system("python3 run.py -V 5 -S 0.3 -LM {} --epochs 100000 -DIR {}".format(model_name, directory))
+os.system("python3 run.py -V 5 -S 0.3 -LM {} --epochs 100000 --preepochs 0 -DIR {}".format(model_name, directory))
