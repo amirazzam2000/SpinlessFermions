ad2e4e6185f02286c490eaed69f5a1629028fe6b
Amir Azzam <ameerazzam123@gmail.com>
2023-04-14 10:20:35 +0000

    resolved conflict

--- run_multiple_copy.py
+++ run_multiple_copy.py
@@ -4,5 +4,5 @@ import os
 model_name = "./partial_data/model-A3"
 os.system("python3 run.py -V 10 -S 0.3 -N 2 -M {} --epochs 100000".format(model_name))
 
-
 os.system("python3 run.py -V 10 -S 0.3 -N 3 -LM {} --epochs 100000 --preepochs 0 -F 1".format(model_name))
+
