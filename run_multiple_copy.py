9e363c7dfad039b746919d38dad7d1753d148012
amirazzam2000 <ameerazzam123@gmail.com>
2023-04-16 12:12:22 +0200

    updating run command

--- run_multiple_copy.py
+++ run_multiple_copy.py
@@ -1,8 +1,14 @@
 import sys
 import os
 
-model_name = "./partial_data/model-A3"
-os.system("python3 run.py -V 10 -S 0.3 -N 2 -M {} --epochs 100000".format(model_name))
+model_name = "./partial_data/model-A3-V5"
+load_name = "./partial_data/model-A3"
+directory= "./results/energy/data/A3_no_pre"
+os.system("python3 run.py -V 5 -S 0.3 -N 3 --epochs 100000 --preepochs 0 -DIR {}".format(directory))
 
-os.system("python3 run.py -V 10 -S 0.3 -N 3 -LM {} --epochs 100000 --preepochs 0 -F 1".format(model_name))
+directory = "./results/energy/data/A3_pre"
+
+os.system("python3 run.py -V 5 -S 0.3 -N 2 -LM {} -M {} --epochs 100000".format(load_name, model_name))
+
+os.system("python3 run.py -V 5 -S 0.3 -N 3 -LM {} --epochs 100000 --preepochs 0 -F 1 -DIR {}".format(model_name, directory))
 
