7e939923194b8d70d0b06f22c2fa9d48d56d0bc5
amirazzam2000 <ameerazzam123@gmail.com>
2023-05-29 10:37:41 +0200

    complete test

--- run_multiple.py
+++ run_multiple.py
@@ -1,8 +1,8 @@
 import sys, os
 
-directory = "results/energy/data/No_Early_Stopping_Testing"
+directory = "results/energy/data/No_Early_Stopping_Testing_complete"
 
-out_dir = "./out/NoES2"
+out_dir = "./out/NoES_complete"
 
 if not os.path.exists(directory):
     os.system("mkdir {}".format(directory))
@@ -13,29 +13,98 @@ if not os.path.exists("./out"):
 if not os.path.exists(out_dir):
     os.system("mkdir {}".format(out_dir))
 
-# no pre no freezing 
-os.system("python3 run.py -V 5 -S 0.3 -NoES -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' > {}/no_trans_no_freezing_no_MH.txt".format(directory, out_dir))
+# no WMH no trans no freezing
 
-model_name = "partial_data/model-OG"
-os.system("python3 run.py -V 0 -S 0.5 -NoES -M {} -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' > {}/dumb.txt".format(model_name, directory, out_dir))
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/no_WMH_no_trans_no_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
 
-# transfer learning (no freezing)
-os.system("python3 run.py -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_no_freezing_no_MH.txt".format(model_name, directory, out_dir))
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' > {}/no_trans_no_freezing_no_MH.txt".format(directory, out_dir))
 
-# transfer learning (freezing)
-os.system("python3 run.py -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_no_MH.txt".format(model_name, directory, out_dir))
+# no WMH no trans w freezing
 
-# weighted MH [no trans] (no freezing)
-os.system("python3 run.py -V 5 -S 0.3 -NoES  -DIR {} -UL 100 -LL 10 --epochs 100000 -T 'NoES_' > {}/no_trans_no_freezing_w_MH.txt".format(directory, out_dir))
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/no_WMH_no_trans_w_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
 
-# with transfer learning :
+model_name = "partial_data/model-A2-for-freezing"
+os.system("python3 run.py -A 2 -V 5 -S 0.3 -NoES -M {} -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' > {}/dumb.txt".format(model_name, directory, out_dir))
 
-# weighted MH (no freezing)
-os.system("python3 run.py -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 100 -LL 10 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_no_freezing_w_MH.txt".format(model_name, directory, out_dir))
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/no_trans_w_freezing_no_MH.txt".format(model_name, directory, out_dir))
 
-# weighted MH (freezing)
-os.system("python3 run.py -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 100 -LL 10 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_w_MH.txt".format(model_name, directory, out_dir))
+# no WMH w trans no freezing
 
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/no_WMH_w_trans_no_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+model_name = "partial_data/model-A4-for-trans"
+os.system("python3 run.py -A 4 -V 0 -S 0.5 -NoES -M {} -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' >> {}/dumb.txt".format(model_name, directory, out_dir))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_no_freezing_no_MH.txt".format(model_name, directory, out_dir))
+
+# w WMH no trans no freezing
+
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/w_WMH_no_trans_no_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES -DIR {} -UL 10 -LL 100 --epochs 100000 -T 'NoES_' > {}/no_trans_no_freezing_w_MH.txt".format(directory, out_dir))
+
+# w WMH no trans w freezing
+
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/w_WMH_no_trans_w_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+model_name = "partial_data/model-A2-for-freezing-w-MH"
+os.system("python3 run.py -A 2 -V 5 -S 0.3 -NoES -M {} -DIR {} -UL 10 -LL 100 --epochs 100000 -T 'NoES_' >> {}/dumb.txt".format(model_name, directory, out_dir))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 10 -LL 100 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/no_trans_w_freezing_w_MH.txt".format(model_name, directory, out_dir))
+
+
+# w WMH w trans no freezing
+
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/w_WMH_w_trans_no_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+model_name = "partial_data/model-A4-for-trans"
+os.system("python3 run.py -A 4 -V 0 -S 0.5 -NoES -M {} -DIR {} -UL 10 -LL 100 --epochs 100000 -T 'NoES_' >> {}/dumb.txt".format(model_name, directory, out_dir))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 10 -LL 100 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_no_freezing_w_MH.txt".format(model_name, directory, out_dir))
+
+
+# w WMH w trans w freezing
+
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/w_WMH_w_trans_w_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+model_name = "partial_data/model-A2-for-freezing-for-trans-w-MH"
+os.system("python3 run.py -A 2 -V 0 -S 0.5 -NoES -M {} -DIR {} -UL 10 -LL 100 --epochs 100000 -T 'NoES_' >> {}/dumb.txt".format(model_name, directory, out_dir))
+
+model_name_f = "partial_data/model-A2-after-freezing-for-trans-w-MH"
+os.system("python3 run.py -A 4 -V 0 -S 0.5 -NoES  -M {} -LM {} -DIR {} -UL 10 -LL 100 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part1.txt".format(model_name_f, model_name, directory, out_dir))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 10 -LL 100 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part2.txt".format(model_name_f, directory, out_dir))
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 10 -LL 100 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part2.txt".format(model_name_f, directory, out_dir))
+
+
+# no WMH w trans w freezing
+
+directory = "results/energy/data/No_Early_Stopping_Testing_complete/no_WMH_w_trans_w_freezing"
+if not os.path.exists(directory):
+    os.system("mkdir {}".format(directory))
+
+model_name = "partial_data/model-A2-for-freezing-for-trans-no-MH"
+os.system("python3 run.py -A 2 -V 0 -S 0.5 -NoES -M {} -DIR {} -UL 1 -LL 1 --epochs 100000 -T 'NoES_' >> {}/dumb.txt".format(model_name, directory, out_dir))
+
+model_name_f = "partial_data/model-A2-after-freezing-for-trans-no-MH"
+os.system("python3 run.py -A 4 -V 0 -S 0.5 -NoES  -M {} -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part1.txt".format(model_name_f, model_name, directory, out_dir))
+
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part2.txt".format(model_name_f, directory, out_dir))
+os.system("python3 run.py -A 4 -V 5 -S 0.3 -NoES  -LM {} -DIR {} -UL 1 -LL 1 --epochs 100000 --preepochs 0 -F -T 'NoES_' > {}/w_trans_w_freezing_w_MH_part2.txt".format(model_name_f, directory, out_dir))
 
 
 
