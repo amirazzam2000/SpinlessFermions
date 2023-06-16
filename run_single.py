20318ab3eb0b68370a55e768408347b70a895d41
amirazzam2000 <ameerazzam123@gmail.com>
2023-06-16 11:31:18 +0200

    updating the test

--- run_single.py
+++ run_single.py
@@ -5,7 +5,7 @@ import os
 Enable_ES = True
 num_particles = 4
 num_pre_particles = 2
-num_epochs = 10000
+num_epochs = 100000
 tag = "Single_ES_A4"
 model_name_tag = "-Single-ES-A4"
 directory_base = "results/energy/data/" + tag
