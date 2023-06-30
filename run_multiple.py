5de764998c2a9a5538f2db6d133f8e626a9a8b7b
amirazzam2000 <ameerazzam123@gmail.com>
2023-06-30 21:29:28 +0200

    run all

--- run_multiple.py
+++ run_multiple.py
@@ -2,13 +2,13 @@ import sys
 import os
 
 
-tag = "NoES_29_Jun"
+tag = "NoES_30_Jun"
 Enable_ES = False
 num_particles = 4
 num_pre_particles = 2
 directory_base = "results/energy/data/" + tag 
 out_dir = "./out/" + tag 
-model_name_tag = "-NoES-29-Jun"
+model_name_tag = "-NoES-30-Jun"
 
 
 noes = "" if Enable_ES else "-NoES" 
